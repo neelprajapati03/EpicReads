cab8c1eba8a343f4895e3350a0cb2d352b1d89ec
NeelPrajapati <neelprajapati.011@gmail.com>
2025-12-22 12:18:42 +0530

    added book validation

---
+++ backend/src/Dtos/types/Book.dto.ts
@@ -0,0 +1,26 @@
+import { PartialType } from "@nestjs/swagger";
+import { IsOptional, IsString, IsNumber } from "class-validator";
+
+export class CreateBookDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  author: string;
+
+  @IsString()
+  category: string;
+
+  @IsString()   // ✅ numeric
+  price: string;
+
+  @IsOptional()
+  @IsNumber()
+  stock?: number;
+
+  @IsOptional()
+  @IsString()
+  imgUrl?: string;
+}
+
+export class UpdateBookDto extends PartialType(CreateBookDto){}
